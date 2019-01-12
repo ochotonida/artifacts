8d0972cdc61290e6a7c4567017274243512104ed
Wouter <wouter@grijsloke.be>
2019-01-12 20:40:44 +0100

    Initial commit

---
+++ src/main/java/artifacts/Artifacts.java
@@ -0,0 +1,16 @@
+package artifacts;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static artifacts.Artifacts.MODID;
+import static artifacts.Artifacts.MODNAME;
+import static artifacts.Artifacts.VERSION;
+
+@Mod(modid = MODID, name = MODNAME, version = VERSION, updateJSON = "https://github.com/ochotonida/artifacts/blob/master/update.json")
+public class Artifacts {
+
+    public static final String MODID = "artifacts";
+    public static final String MODNAME = "Artifacts";
+    public static final String VERSION = "1.12.2-0.0.0";
+    
+}
