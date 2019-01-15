9869b3d14c801bcd187e63ede23ad0288ba911a0
Wouter <wouter@grijsloke.be>
2019-01-15 11:28:06 +0100

    Added snorkel rendering

---
+++ src/main/java/artifacts/client/model/ModelSnorkel.java
@@ -0,0 +1,48 @@
+package artifacts.client.model;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public class ModelSnorkel extends ModelBase {
+    public ModelRenderer snorkelbox1;
+    public ModelRenderer snorkelbox2;
+    public ModelRenderer head;
+    public ModelRenderer headOverlay;
+
+    public ModelSnorkel() {
+        this.textureWidth = 64;
+        this.textureHeight = 32;
+        this.snorkelbox1 = new ModelRenderer(this, 0, 14);
+        this.snorkelbox1.addBox(-2, 2, -6, 8, 2, 2, 0);
+        this.head = new ModelRenderer(this, 28, 0);
+        this.headOverlay = new ModelRenderer(this, 28, 16);
+        this.head.addBox(-4, -3.5F, -4, 8, 8, 8, 0);
+        this.headOverlay.addBox(-4, -3.5F, -4, 8, 8, 8, 0);
+        this.snorkelbox2 = new ModelRenderer(this, 0, 0);
+        this.snorkelbox2.addBox(4.0F, -2.0F, -5, 2, 2, 12, 0);
+        snorkelbox2.rotateAngleX = 0.7853981633974483F;
+    }
+
+    @Override
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float scale) {
+        this.snorkelbox1.render(scale);
+        this.snorkelbox2.render(scale);
+        if (((EntityPlayer) entity).getItemStackFromSlot(EntityEquipmentSlot.HEAD).isEmpty()) {
+            GlStateManager.pushMatrix();
+
+            GlStateManager.scale(1.1875, 1.1875, 1.1875);
+            this.head.render(scale);
+
+            GlStateManager.enableBlend();
+            GlStateManager.color(1, 1, 1, 0.3F);
+            this.headOverlay.render(scale);
+            GlStateManager.disableBlend();
+
+            GlStateManager.popMatrix();
+        }
+    }
+}
---
+++ src/main/java/artifacts/common/item/BaubleSnorkel.java
@@ -0,0 +1,46 @@
+package artifacts.common.item;
+
+import artifacts.Artifacts;
+import artifacts.client.model.ModelSnorkel;
+import baubles.api.BaubleType;
+import baubles.api.render.IRenderBauble;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+public class BaubleSnorkel extends BaublePotionEffect implements IRenderBauble {
+
+    public static final ModelBase MODEL = new ModelSnorkel();
+    public static final ResourceLocation TEXTURES = new ResourceLocation(Artifacts.MODID,"textures/entity/snorkel.png");
+
+    public BaubleSnorkel() {
+        super("bauble_snorkel", BaubleType.HEAD, MobEffects.WATER_BREATHING, 0);
+        setEquipSound(SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 1);
+    }
+
+    @Override
+    public void onPlayerBaubleRender(ItemStack stack, EntityPlayer player, RenderType renderType, float partialTicks) {
+        if(renderType == RenderType.HEAD) {
+            GlStateManager.pushMatrix();
+
+            Helper.translateToHeadLevel(player);
+            Helper.translateToFace();
+            Helper.defaultTransforms();
+
+            GlStateManager.rotate(180, 0, 0, 1);
+            GlStateManager.scale(1/0.55F, 1/0.55F, 1/0.55F);
+            GlStateManager.scale(1/16F, 1/16F, 1/16F);
+            GlStateManager.translate(0, 0, 4);
+
+            Minecraft.getMinecraft().renderEngine.bindTexture(TEXTURES);
+            MODEL.render(player, 0, 0, 0, 0, 0, 1);
+
+            GlStateManager.popMatrix();
+        }
+    }
+}
