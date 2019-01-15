3eb90294d0ae5ff8adc1a63c9cffc5159c5464d4
Wouter <wouter@grijsloke.be>
2019-01-15 23:01:17 +0100

    Added lucky horseshoe

--- src/main/java/artifacts/ModItems.java
+++ src/main/java/artifacts/ModItems.java
@@ -1,9 +1,6 @@
 package artifacts;
 
-import artifacts.common.item.BaubleAmulet;
-import artifacts.common.item.BaublePotionEffect;
-import artifacts.common.item.BaubleBase;
-import artifacts.common.item.BaubleSnorkel;
+import artifacts.common.item.*;
 import baubles.api.BaubleType;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
@@ -18,6 +15,7 @@ public class ModItems {
     public static BaubleBase baubleLightningAmulet = new BaubleAmulet("bauble_lightning_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/lightning_amulet.png"));
     public static BaubleBase baubleFireAmulet = new BaubleAmulet("bauble_fire_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/fire_amulet.png"));
     public static BaubleBase baubleThornsAmulet = new BaubleAmulet("bauble_thorns_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/thorns_amulet.png"));
+    public static BaubleBase baubleHorseshoe = new BaubleHorseshoe();
 
     public static void registerItems(IForgeRegistry<Item> registry) {
         registry.registerAll(
@@ -25,7 +23,8 @@ public class ModItems {
                 baubleSnorkel,
                 baubleLightningAmulet,
                 baubleFireAmulet,
-                baubleThornsAmulet
+                baubleThornsAmulet,
+                baubleHorseshoe
         );
     }
 
@@ -35,5 +34,6 @@ public class ModItems {
         Artifacts.proxy.registerItemRenderer(baubleLightningAmulet, 0, baubleLightningAmulet.name);
         Artifacts.proxy.registerItemRenderer(baubleFireAmulet, 0, baubleFireAmulet.name);
         Artifacts.proxy.registerItemRenderer(baubleThornsAmulet, 0, baubleThornsAmulet.name);
+        Artifacts.proxy.registerItemRenderer(baubleHorseshoe, 0, baubleHorseshoe.name);
     }
 }
---
+++ src/main/java/artifacts/common/item/BaubleHorseshoe.java
@@ -0,0 +1,41 @@
+package artifacts.common.item;
+
+import artifacts.ModItems;
+import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class BaubleHorseshoe extends BaubleBase {
+
+    public BaubleHorseshoe() {
+        super("bauble_horseshoe", BaubleType.CHARM);
+        setEquipSound(SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1);
+
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public static void onLivingFall(LivingFallEvent event) {
+        if (event.getEntity() instanceof EntityPlayer) {
+            if (BaublesApi.isBaubleEquipped((EntityPlayer) event.getEntity(), ModItems.baubleHorseshoe) != -1) {
+                if (event.getDistance() > 5) {
+                    PotionEffect potioneffect = event.getEntityLiving().getActivePotionEffect(MobEffects.JUMP_BOOST);
+                    float f = potioneffect == null ? 0.0F : (float)(potioneffect.getAmplifier() + 1);
+                    int i = MathHelper.ceil((event.getDistance() - 3.0F - f) * event.getDamageMultiplier());
+                    if (i > 0) {
+                        event.getEntity().playSound(SoundEvents.ENTITY_GENERIC_SMALL_FALL, 1, 1);
+                    }
+                }
+                event.setCanceled(true);
+            }
+        }
+    }
+}
