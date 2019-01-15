6aa9a4584607e0aec12ad959b26aa4bb1a71a3c9
Wouter <wouter@grijsloke.be>
2019-01-15 21:06:47 +0100

    Added flame & thorn pendants

--- src/main/java/artifacts/ModItems.java
+++ src/main/java/artifacts/ModItems.java
@@ -1,6 +1,6 @@
 package artifacts;
 
-import artifacts.common.item.BaubleLightningAmulet;
+import artifacts.common.item.BaubleAmulet;
 import artifacts.common.item.BaublePotionEffect;
 import artifacts.common.item.BaubleBase;
 import artifacts.common.item.BaubleSnorkel;
@@ -8,19 +8,24 @@ import baubles.api.BaubleType;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 
     public static BaubleBase baubleBalloon = new BaublePotionEffect("bauble_balloon", BaubleType.RING, MobEffects.JUMP_BOOST, 1).setEquipSound(SoundEvents.ENTITY_ITEM_PICKUP, 0.5F);
     public static BaubleBase baubleSnorkel = new BaubleSnorkel();
-    public static BaubleBase baubleLightningAmulet = new BaubleLightningAmulet();
+    public static BaubleBase baubleLightningAmulet = new BaubleAmulet("bauble_lightning_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/lightning_amulet.png"));
+    public static BaubleBase baubleFireAmulet = new BaubleAmulet("bauble_fire_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/fire_amulet.png"));
+    public static BaubleBase baubleThornsAmulet = new BaubleAmulet("bauble_thorns_amulet", new ResourceLocation(Artifacts.MODID,"textures/entity/thorns_amulet.png"));
 
     public static void registerItems(IForgeRegistry<Item> registry) {
         registry.registerAll(
                 baubleBalloon,
                 baubleSnorkel,
-                baubleLightningAmulet
+                baubleLightningAmulet,
+                baubleFireAmulet,
+                baubleThornsAmulet
         );
     }
 
@@ -28,5 +33,7 @@ public class ModItems {
         Artifacts.proxy.registerItemRenderer(baubleBalloon, 0, baubleBalloon.name);
         Artifacts.proxy.registerItemRenderer(baubleSnorkel, 0, baubleSnorkel.name);
         Artifacts.proxy.registerItemRenderer(baubleLightningAmulet, 0, baubleLightningAmulet.name);
+        Artifacts.proxy.registerItemRenderer(baubleFireAmulet, 0, baubleFireAmulet.name);
+        Artifacts.proxy.registerItemRenderer(baubleThornsAmulet, 0, baubleThornsAmulet.name);
     }
 }
---
+++ src/main/java/artifacts/common/item/BaubleAmulet.java
@@ -0,0 +1,86 @@
+package artifacts.common.item;
+
+import artifacts.ModItems;
+import artifacts.client.model.ModelAmulet;
+import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
+import baubles.api.render.IRenderBauble;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
+
+@Mod.EventBusSubscriber
+public class BaubleAmulet extends BaubleBase implements IRenderBauble {
+
+    protected ModelBase model = new ModelAmulet();
+
+    protected ResourceLocation textures;
+
+    public BaubleAmulet(String name, ResourceLocation textures) {
+        super(name, BaubleType.AMULET);
+        this.textures = textures;
+    }
+
+    @Override
+    public void onPlayerBaubleRender(ItemStack stack, EntityPlayer player, IRenderBauble.RenderType renderType, float partialTicks) {
+        if (renderType == RenderType.BODY) {
+            Helper.rotateIfSneaking(player);
+            Minecraft.getMinecraft().renderEngine.bindTexture(textures);
+            model.render(player, 0, 0, 0, 0, 0, 1/16F);
+        }
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public static void onLivingHurt(LivingHurtEvent event) {
+        if (!event.getEntity().world.isRemote && event.getEntity() instanceof EntityPlayer) {
+
+            // lightning amulet
+            if (BaublesApi.isBaubleEquipped((EntityPlayer) event.getEntity(), ModItems.baubleLightningAmulet) != -1) {
+                if (event.getSource() == DamageSource.LIGHTNING_BOLT) {
+                    event.setCanceled(true);
+                } else if (event.getSource().getTrueSource() instanceof EntityLiving && !((EntityPlayer) event.getEntity()).getCooldownTracker().hasCooldown(ModItems.baubleLightningAmulet)) {
+                    EntityLiving attacker = (EntityLiving) event.getSource().getTrueSource();
+                    if (attacker.world.canSeeSky(attacker.getPosition())) {
+                        attacker.world.addWeatherEffect(new EntityLightningBolt(attacker.world, attacker.posX, attacker.posY, attacker.posZ, false));
+                        ((EntityPlayer) event.getEntity()).getCooldownTracker().setCooldown(ModItems.baubleLightningAmulet, 320);
+                    }
+                }
+            }
+
+            // fire amulet
+            if (BaublesApi.isBaubleEquipped((EntityPlayer) event.getEntity(), ModItems.baubleFireAmulet) != -1) {
+                if (event.getSource().getTrueSource() instanceof EntityLiving && !((EntityPlayer) event.getEntity()).getCooldownTracker().hasCooldown(ModItems.baubleFireAmulet)) {
+                    EntityLiving attacker = (EntityLiving) event.getSource().getTrueSource();
+                    if (!attacker.isImmuneToFire() && attacker.attackable()) {
+                        attacker.setFire(4);
+                        attacker.attackEntityFrom(new EntityDamageSource("onFire", event.getEntity()).setFireDamage(), 2);
+                        ((EntityPlayer) event.getEntity()).getCooldownTracker().setCooldown(ModItems.baubleFireAmulet, 160);
+                    }
+                }
+            }
+
+            // thorns amulet
+            if (BaublesApi.isBaubleEquipped((EntityPlayer) event.getEntity(), ModItems.baubleThornsAmulet) != -1) {
+                if (event.getSource().getTrueSource() instanceof EntityLiving) {
+                    EntityLiving attacker = (EntityLiving) event.getSource().getTrueSource();
+                    Random random = ((EntityPlayer) event.getEntity()).getRNG();
+                    if (attacker.attackable() && random.nextFloat() < 0.45) {
+                        attacker.attackEntityFrom(DamageSource.causeThornsDamage(event.getEntity()), 1 + random.nextInt(4));
+                    }
+                }
+            }
+        }
+    }
+}
