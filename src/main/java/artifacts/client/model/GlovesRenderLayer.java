3a266a145d60bf8906c8d0b1743d7b8346d2d545
Wouter <wouter@grijsloke.be>
2019-01-30 22:09:09 +0100

    Glowy gauntlet 🔦

--- src/main/java/artifacts/client/model/GlovesRenderLayer.java
+++ src/main/java/artifacts/client/model/GlovesRenderLayer.java
@@ -9,6 +9,7 @@ import baubles.common.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.player.EntityPlayer;
@@ -29,15 +30,17 @@ public class GlovesRenderLayer implements LayerRenderer<EntityPlayer> {
     public final ResourceLocation powerGloveTextures;
     public final ResourceLocation mechanicalGloveTextures;
     public final ResourceLocation fireGauntletTextures;
+    public final ResourceLocation fireGauntletOverlayTextures;
 
     public GlovesRenderLayer(boolean smallArms, RenderPlayer renderPlayer) {
         this.renderPlayer = renderPlayer;
-        this.model = new ModelPlayer(0.5F, smallArms);
+        model = new ModelPlayer(0.5F, smallArms);
         model.setVisible(false);
-        this.feralClawsTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/feral_claws_" + (smallArms ? "slim" : "normal") + ".png");
-        this.powerGloveTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/power_glove_" + (smallArms ? "slim" : "normal") + ".png");
-        this.mechanicalGloveTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/mechanical_glove_" + (smallArms ? "slim" : "normal") + ".png");
-        this.fireGauntletTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/fire_gauntlet_" + (smallArms ? "slim" : "normal") + ".png");
+        feralClawsTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/feral_claws_" + (smallArms ? "slim" : "normal") + ".png");
+        powerGloveTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/power_glove_" + (smallArms ? "slim" : "normal") + ".png");
+        mechanicalGloveTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/mechanical_glove_" + (smallArms ? "slim" : "normal") + ".png");
+        fireGauntletTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/fire_gauntlet_" + (smallArms ? "slim" : "normal") + ".png");
+        fireGauntletOverlayTextures = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/fire_gauntlet_overlay_" + (smallArms ? "slim" : "normal") + ".png");
     }
 
     @Override
@@ -48,6 +51,13 @@ public class GlovesRenderLayer implements LayerRenderer<EntityPlayer> {
 
         GlStateManager.enableLighting();
         GlStateManager.enableRescaleNormal();
+
+        float lastLightmapX = OpenGlHelper.lastBrightnessX;
+        float lastLightmapY = OpenGlHelper.lastBrightnessY;
+
+        int light = 15728880;
+        int lightmapX = light % 65536;
+        int lightmapY = light / 65536;
 
         model.setModelAttributes(renderPlayer.getMainModel());
         model.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale, player);
@@ -60,6 +70,13 @@ public class GlovesRenderLayer implements LayerRenderer<EntityPlayer> {
             model.bipedLeftArm.showModel = true;
             model.bipedLeftArmwear.showModel = true;
             model.render(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            textures = getOverlayTextures(baubleHandler.getStackInSlot(BaubleType.RING.getValidSlots()[0]));
+            if (textures != null) {
+                Minecraft.getMinecraft().getTextureManager().bindTexture(textures);
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightmapX, lightmapY);
+                model.render(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lastLightmapX, lastLightmapY);
+            }
             model.bipedLeftArmwear.showModel = false;
             model.bipedLeftArm.showModel = false;
         }
@@ -69,6 +86,13 @@ public class GlovesRenderLayer implements LayerRenderer<EntityPlayer> {
             model.bipedRightArm.showModel = true;
             model.bipedRightArmwear.showModel = true;
             model.render(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+            textures = getOverlayTextures(baubleHandler.getStackInSlot(BaubleType.RING.getValidSlots()[1]));
+            if (textures != null) {
+                Minecraft.getMinecraft().getTextureManager().bindTexture(textures);
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lightmapX, lightmapY);
+                model.render(player, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, lastLightmapX, lastLightmapY);
+            }
             model.bipedRightArmwear.showModel = false;
             model.bipedRightArm.showModel = false;
         }
@@ -83,6 +107,14 @@ public class GlovesRenderLayer implements LayerRenderer<EntityPlayer> {
             return mechanicalGloveTextures;
         } else if (stack.getItem() == ModItems.FIRE_GAUNTLET) {
             return fireGauntletTextures;
+        }
+        return null;
+    }
+
+    private @Nullable
+    ResourceLocation getOverlayTextures(ItemStack stack) {
+        if (stack.getItem() == ModItems.FIRE_GAUNTLET) {
+            return fireGauntletOverlayTextures;
         }
         return null;
     }
