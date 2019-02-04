7948ea8155047fee807dc010ec2338a1432e6b9d
Wouter <wouter@grijsloke.be>
2019-02-04 17:28:04 +0100

    Easter eggs 🥚

--- src/main/java/artifacts/client/model/LayerDrinkingHat.java
+++ src/main/java/artifacts/client/model/LayerDrinkingHat.java
@@ -16,6 +16,7 @@ import javax.annotation.Nonnull;
 public class LayerDrinkingHat extends LayerBauble {
 
     public static final ResourceLocation TEXTURES = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/drinking_hat.png");
+    public static final ResourceLocation SPECIAL_TEXTURES = new ResourceLocation(Artifacts.MODID, "textures/entity/layer/drinking_hat_special.png");
 
     protected ModelRenderer hat;
     protected ModelRenderer canLeft;
@@ -23,11 +24,13 @@ public class LayerDrinkingHat extends LayerBauble {
     protected ModelRenderer strawLeft;
     protected ModelRenderer strawRight;
     protected ModelRenderer strawMiddle;
+    protected ModelRenderer hatShade;
 
     public LayerDrinkingHat(RenderPlayer renderPlayer) {
         super(renderPlayer);
 
         hat = new ModelRenderer(model, 26, 41);
+        hatShade = new ModelRenderer(model, 0, 52);
         canLeft = new ModelRenderer(model, 0, 41);
         canRight = new ModelRenderer(model, 12, 41);
         strawLeft = new ModelRenderer(model, 0, 32);
@@ -35,11 +38,12 @@ public class LayerDrinkingHat extends LayerBauble {
         strawMiddle = new ModelRenderer(model, 0, 50);
 
         hat.addBox(-4, -8, -4, 8, 8, 8, 1);
-        canLeft.addBox(4, -11, -1, 3, 6, 3, 0);
-        canRight.addBox(-7, -11, -1, 3, 6, 3, 0);
-        strawLeft.addBox(5, -4, -3, 1, 1, 8, 0);
-        strawRight.addBox(-6, -4, -3, 1, 1, 8, 0);
-        strawMiddle.addBox(-6, -1, -5, 12, 1, 1, 0);
+        hatShade.addBox(-4, -6, -8, 8, 1, 4, 0);
+        canLeft.addBox(4, -11, -1, 3, 6, 3);
+        canRight.addBox(-7, -11, -1, 3, 6, 3);
+        strawLeft.addBox(5, -4, -3, 1, 1, 8);
+        strawRight.addBox(-6, -4, -3, 1, 1, 8);
+        strawMiddle.addBox(-6, -1, -5, 12, 1, 1);
 
         strawLeft.rotateAngleX = 0.7853F;
         strawRight.rotateAngleX = 0.7853F;
@@ -50,6 +54,7 @@ public class LayerDrinkingHat extends LayerBauble {
         model.bipedHead.addChild(canRight);
         model.bipedHead.addChild(strawLeft);
         model.bipedHead.addChild(strawMiddle);
+        model.bipedHead.addChild(hatShade);
 
         model.bipedHead.showModel = true;
     }
@@ -64,7 +69,13 @@ public class LayerDrinkingHat extends LayerBauble {
             GlStateManager.translate(0, 0.2F, 0);
         }
 
-        Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURES);
+        if (player.getName().equals("wouterke")) {
+            Minecraft.getMinecraft().getTextureManager().bindTexture(SPECIAL_TEXTURES);
+            hatShade.showModel = player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).isEmpty();
+        } else {
+            Minecraft.getMinecraft().getTextureManager().bindTexture(TEXTURES);
+            hatShade.showModel = false;
+        }
 
         hat.showModel = player.getItemStackFromSlot(EntityEquipmentSlot.HEAD).isEmpty();
 
