2578b0cb47900d8d2c111147afb82f27fb1003c7
Wouter <wouter@grijsloke.be>
2019-02-07 11:51:07 +0100

    Fixed everlasting food duplication with thermal expansion trivection chamber 🔨

--- src/main/java/artifacts/common/item/ItemEverlastingFood.java
+++ src/main/java/artifacts/common/item/ItemEverlastingFood.java
@@ -14,11 +14,15 @@ import net.minecraft.entity.passive.*;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.stats.StatList;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootEntryItem;
@@ -26,23 +30,59 @@ import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import squeek.applecore.api.food.FoodValues;
+import squeek.applecore.api.food.IEdible;
+import squeek.applecore.api.food.ItemFoodProxy;
+
+import javax.annotation.ParametersAreNonnullByDefault;
 
 @Mod.EventBusSubscriber
 @SuppressWarnings("unused")
 @MethodsReturnNonnullByDefault
-public class ItemEverlastingFood extends ItemFood {
+@ParametersAreNonnullByDefault
+@Optional.Interface(iface = "squeek.applecore.api.food.IEdible", modid = "applecore")
+public class ItemEverlastingFood extends Item implements IEdible {
 
     public final String name;
+    private final int amount;
+    private final float saturation;
+    private PotionEffect potionEffect;
+    private float potionProbability;
 
     public ItemEverlastingFood(String name, int amount, float saturation) {
-        super(amount, saturation, false);
         setRegistryName(name);
         setUnlocalizedName(Artifacts.MODID + "." + name);
         setCreativeTab(CreativeTabs.FOOD);
         setMaxStackSize(1);
         this.name = name;
+        this.amount = amount;
+        this.saturation = saturation;
+    }
+
+    @Override
+    @Optional.Method(modid = "applecore")
+    public FoodValues getFoodValues(ItemStack itemStack) {
+        return new FoodValues(amount, saturation);
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 32;
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.EAT;
+    }
+
+    protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+        if (!worldIn.isRemote && potionEffect != null && worldIn.rand.nextFloat() < potionProbability) {
+            player.addPotionEffect(new PotionEffect(potionEffect));
+        }
     }
 
     @Override
@@ -61,17 +101,45 @@ public class ItemEverlastingFood extends ItemFood {
 
     @Override
     @SuppressWarnings("NullableProblems")
-    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entityLiving) {
-        if (entityLiving instanceof EntityPlayer) {
-            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
-            entityplayer.getFoodStats().addStats(this, stack);
-            world.playSound(null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
-            this.onFoodEaten(stack, world, entityplayer);
+    public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entity) {
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+
+            if (Loader.isModLoaded("applecore")) {
+                onEatenCompatibility(stack, player);
+            } else {
+                player.getFoodStats().addStats(amount, saturation);
+            }
+
+            world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
+            onFoodEaten(stack, world, player);
             // noinspection ConstantConditions
-            entityplayer.addStat(StatList.getObjectUseStats(this));
+            player.addStat(StatList.getObjectUseStats(this));
         }
 
         return stack;
+    }
+
+    @Optional.Method(modid = "applecore")
+    public void onEatenCompatibility(ItemStack stack, EntityPlayer player) {
+        new ItemFoodProxy(this).onEaten(stack, player);
+    }
+
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+
+        if (playerIn.canEat(false)) {
+            playerIn.setActiveHand(handIn);
+            return new ActionResult<>(EnumActionResult.SUCCESS, itemstack);
+        } else {
+            return new ActionResult<>(EnumActionResult.FAIL, itemstack);
+        }
+    }
+
+    public ItemEverlastingFood setPotionEffect(PotionEffect effect, float probability) {
+        this.potionEffect = effect;
+        this.potionProbability = probability;
+        return this;
     }
 
     @SubscribeEvent
