afafa0399e4871740d36c2cad740df911ed7e21b
Wouter <wouter@grijsloke.be>
2020-07-23 12:05:25 +0200

    🔨 fixed an issue where items were not being added to loot tables

--- src/main/java/artifacts/common/init/LootTables.java
+++ src/main/java/artifacts/common/init/LootTables.java
@@ -30,7 +30,12 @@ public class LootTables {
                 "chests/abandoned_mineshaft",
                 "chests/buried_treasure",
                 "chests/desert_pyramid",
+                "chests/end_city_treasure",
                 "chests/jungle_temple",
+                "chests/nether_bridge",
+                "chests/pillager_outpost",
+                "chests/shipwreck_treasure",
+                "chests/stronghold_corridor",
                 "chests/underwater_ruin_big",
                 "chests/woodland_mansion",
                 "entities/cow"
