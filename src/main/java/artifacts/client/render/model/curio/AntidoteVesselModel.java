e796b2164f98ef496e74252e441139e51d1bfc3a
Wouter <wouter@grijsloke.be>
2020-07-31 09:52:31 +0200

    🎨 updated antidote vessel model & item texture

--- src/main/java/artifacts/client/render/model/curio/AntidoteVesselModel.java
+++ src/main/java/artifacts/client/render/model/curio/AntidoteVesselModel.java
@@ -1,48 +1,29 @@
 package artifacts.client.render.model.curio;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.LivingEntity;
 
 public class AntidoteVesselModel extends BipedModel<LivingEntity> {
 
-    public final ModelRenderer belt;
-    public final ModelRenderer jar;
-
     public AntidoteVesselModel() {
-        super(0.5F, 0, 64, 32);
-
-        setVisible(false);
+        super(0.5F, 0, 32, 32);
 
         bipedBody = new ModelRenderer(this, 0, 0);
-        belt = new ModelRenderer(this, 0, 0);
-        jar = new ModelRenderer(this, 0, 16);
+        ModelRenderer jar = new ModelRenderer(this, 0, 16);
+        ModelRenderer lid = new ModelRenderer(this, 0, 26);
 
-        ModelRenderer lid = new ModelRenderer(this, 24, 0);
+        bipedBody.addBox(-4, 0, -2, 8, 12, 4, 0.5F);
 
-        belt.addBox(-4, 0, -2, 8, 12, 4, 0.5F);
-
-        jar.addBox(0, 0, 0, 6, 8, 6);
-        lid.addBox(1, -1, 1, 4, 1, 4);
-
-        jar.setRotationPoint(6, 18, -10);
+        jar.addBox(-2, 0, -2, 4, 6, 4);
+        lid.addBox(-1, -1, -1, 2, 1, 2);
+        jar.setRotationPoint(4, 9, -3);
         jar.rotateAngleY = -0.5F;
 
         jar.addChild(lid);
-        bipedBody.addChild(belt);
         bipedBody.addChild(jar);
-    }
 
-    @Override
-    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-        jar.showModel = false;
-        bipedBody.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-        jar.showModel = true;
-        matrixStackIn.scale(0.5F, 0.5F, 0.5F);
-        belt.showModel = false;
-        bipedBody.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-        belt.showModel = true;
+        setVisible(false);
+        bipedBody.showModel = true;
     }
 }
