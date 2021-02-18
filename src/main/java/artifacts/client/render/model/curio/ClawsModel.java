8a25b9682e8f4fb45047fcd4339f075158589dd4
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-02-18 15:33:55 +0100

    Tweak claws on slim models

--- src/main/java/artifacts/client/render/model/curio/ClawsModel.java
+++ src/main/java/artifacts/client/render/model/curio/ClawsModel.java
@@ -7,6 +7,8 @@ public class ClawsModel extends GloveModel {
     public ClawsModel(boolean smallArms) {
         super(smallArms);
 
+        int smallArmsOffset = smallArms ? 1 : 0;
+
         ModelRenderer clawLeftUpper1 = new ModelRenderer(this, 0, 6);
         ModelRenderer clawRightUpper1 = new ModelRenderer(this, 0, 38);
         ModelRenderer clawLeftUpper2 = new ModelRenderer(this, 8, 6);
@@ -15,14 +17,14 @@ public class ClawsModel extends GloveModel {
         ModelRenderer clawRightLower1 = new ModelRenderer(this, 0, 32);
         ModelRenderer clawLeftLower2 = new ModelRenderer(this, 8, 0);
         ModelRenderer clawRightLower2 = new ModelRenderer(this, 8, 32);
-        clawLeftUpper1.addBox(3, 10, -1.5F, 1, 4, 1);
-        clawRightUpper1.addBox(-4, 10, -1.5F, 1, 4, 1);
-        clawLeftUpper2.addBox(3, 10, 0.5F, 1, 4, 1);
-        clawRightUpper2.addBox(-4, 10, 0.5F, 1, 4, 1);
-        clawLeftLower1.addBox(0, 10, -1.5F, 3, 5, 1);
-        clawRightLower1.addBox(-3, 10, -1.5F, 3, 5, 1);
-        clawLeftLower2.addBox(0, 10, 0.5F, 3, 5, 1);
-        clawRightLower2.addBox(-3, 10, 0.5F, 3, 5, 1);
+        clawLeftUpper1.addBox(3 - smallArmsOffset, 10, -1.5F, 1, 4, 1);
+        clawRightUpper1.addBox(-4 + smallArmsOffset, 10, -1.5F, 1, 4, 1);
+        clawLeftUpper2.addBox(3 - smallArmsOffset, 10, 0.5F, 1, 4, 1);
+        clawRightUpper2.addBox(-4 + smallArmsOffset, 10, 0.5F, 1, 4, 1);
+        clawLeftLower1.addBox(-smallArmsOffset, 10, -1.5F, 3, 5, 1);
+        clawRightLower1.addBox(-3 + smallArmsOffset, 10, -1.5F, 3, 5, 1);
+        clawLeftLower2.addBox(-smallArmsOffset, 10, 0.5F, 3, 5, 1);
+        clawRightLower2.addBox(-3 + smallArmsOffset, 10, 0.5F, 3, 5, 1);
         bipedLeftArm.addChild(clawLeftUpper1);
         bipedRightArm.addChild(clawRightUpper1);
         bipedLeftArm.addChild(clawLeftUpper2);
