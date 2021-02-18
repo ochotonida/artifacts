0f374705ee33758e9fd8a53626753c046d0e34e9
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-02-18 12:53:17 +0100

    Update steadfast spikes model & textures

--- src/main/java/artifacts/client/render/model/curio/SteadfastSpikesModel.java
+++ src/main/java/artifacts/client/render/model/curio/SteadfastSpikesModel.java
@@ -10,30 +10,18 @@ public class SteadfastSpikesModel extends PlayerModel<LivingEntity> {
     public SteadfastSpikesModel() {
         super(0.5F, false);
 
-        ModelRenderer clawLeftLower1 = new ModelRenderer(this, 32, 0);
-        ModelRenderer clawRightLower1 = new ModelRenderer(this, 32, 32);
-        ModelRenderer clawLeftLower2 = new ModelRenderer(this, 44, 0);
-        ModelRenderer clawRightLower2 = new ModelRenderer(this, 44, 32);
-        ModelRenderer clawLeftUpper1 = new ModelRenderer(this, 32, 8);
-        ModelRenderer clawRightUpper1 = new ModelRenderer(this, 32, 40);
-        ModelRenderer clawLeftUpper2 = new ModelRenderer(this, 44, 8);
-        ModelRenderer clawRightUpper2 = new ModelRenderer(this, 44, 40);
-        clawLeftLower1.addBox(-1.5F, 10, -7, 1, 3, 5);
-        clawRightLower1.addBox(-1.5F, 10, -7, 1, 3, 5);
-        clawLeftLower2.addBox(0.5F, 10, -7, 1, 3, 5);
-        clawRightLower2.addBox(0.5F, 10, -7, 1, 3, 5);
-        clawLeftUpper1.addBox(-1.5F, 9, -6, 1, 1, 4);
-        clawRightUpper1.addBox(-1.5F, 9, -6, 1, 1, 4);
-        clawLeftUpper2.addBox(0.5F, 9, -6, 1, 1, 4);
-        clawRightUpper2.addBox(0.5F, 9, -6, 1, 1, 4);
-        bipedLeftLeg.addChild(clawLeftLower1);
-        bipedRightLeg.addChild(clawRightLower1);
-        bipedLeftLeg.addChild(clawLeftLower2);
-        bipedRightLeg.addChild(clawRightLower2);
-        bipedLeftLeg.addChild(clawLeftUpper1);
-        bipedRightLeg.addChild(clawRightUpper1);
-        bipedLeftLeg.addChild(clawLeftUpper2);
-        bipedRightLeg.addChild(clawRightUpper2);
+        ModelRenderer clawLeft1 = new ModelRenderer(this, 32, 0);
+        ModelRenderer clawRight1 = new ModelRenderer(this, 43, 0);
+        ModelRenderer clawLeft2 = new ModelRenderer(this, 32, 8);
+        ModelRenderer clawRight2 = new ModelRenderer(this, 43, 8);
+        clawLeft1.addBox(-1.5F, 9, -7, 1, 3, 5);
+        clawRight1.addBox(-1.5F, 9, -7, 1, 3, 5);
+        clawLeft2.addBox(0.5F, 9, -7, 1, 3, 5);
+        clawRight2.addBox(0.5F, 9, -7, 1, 3, 5);
+        bipedLeftLeg.addChild(clawLeft1);
+        bipedRightLeg.addChild(clawRight1);
+        bipedLeftLeg.addChild(clawLeft2);
+        bipedRightLeg.addChild(clawRight2);
 
         setVisible(false);
         bipedLeftLeg.showModel = true;
