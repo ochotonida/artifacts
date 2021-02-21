91264918b57d557476b59e27263c3709081e1998
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-02-21 15:21:39 +0100

    Revert 5b734049

--- src/main/java/artifacts/common/item/HeliumFlamingoItem.java
+++ src/main/java/artifacts/common/item/HeliumFlamingoItem.java
@@ -4,11 +4,14 @@ import artifacts.Artifacts;
 import artifacts.client.render.model.curio.HeliumFlamingoModel;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
@@ -17,6 +20,16 @@ import javax.annotation.Nonnull;
 public class HeliumFlamingoItem extends CurioItem {
 
     private static final ResourceLocation TEXTURE = new ResourceLocation(Artifacts.MODID, "textures/entity/curio/helium_flamingo.png");
+
+    public HeliumFlamingoItem() {
+        MinecraftForge.EVENT_BUS.addListener(this::onEntityJoinWorld);
+    }
+
+    private void onEntityJoinWorld(EntityJoinWorldEvent event) {
+        if (event.getEntity() instanceof ItemEntity && ((ItemEntity) event.getEntity()).getItem().getItem() == this) {
+            event.getEntity().setNoGravity(true);
+        }
+    }
 
     @Nonnull
     @Override
