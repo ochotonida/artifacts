b1d7ce394e306c4cefb898f831a027d4cb585052
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-04-15 19:18:13 +0200

    Update charm of sinking model

--- src/main/java/artifacts/client/render/model/curio/CharmOfSinkingModel.java
+++ src/main/java/artifacts/client/render/model/curio/CharmOfSinkingModel.java
@@ -1,8 +1,28 @@
 package artifacts.client.render.model.curio;
 
-public class CharmOfSinkingModel extends PendantModel {
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.LivingEntity;
+
+public class CharmOfSinkingModel extends BipedModel<LivingEntity> {
 
     public CharmOfSinkingModel() {
-        body.texOffs(56, 0).addBox(0, 3.5F, -5, 1, 1, 1);
+        super(RenderType::entityTranslucent, 0, 0, 64, 64);
+        body = new ModelRenderer(this, 0, 0);
+        body.addBox(-(2 * 8) / 2F, -1 / 2F, -(2 * 4 + 1) / 2F, 2 * 8, 2 * 12 + 1, 2 * 4 + 1);
+        // gem
+        body.texOffs(50, 0);
+        body.addBox(-1, 3.5F, -5, 2, 4, 1);
+        setAllVisible(false);
+        body.visible = true;
+    }
+
+    @Override
+    public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int light, int overlay, float red, float green, float blue, float alpha) {
+        matrixStack.scale(0.5F, 0.5F, 0.5F);
+        body.render(matrixStack, buffer, light, overlay, red, green, blue, alpha);
     }
 }
