9e6d7c8be12a7eb263b17c7534e0808dcdffbc9a
ochotonida <37985539+ochotonida@users.noreply.github.com>
2021-05-16 02:33:37 +0200

    Add quark mimics, fix config translation keys

--- src/main/java/artifacts/client/render/MimicChestLayer.java
+++ src/main/java/artifacts/client/render/MimicChestLayer.java
@@ -2,6 +2,7 @@ package artifacts.client.render;
 
 import artifacts.client.render.model.entity.MimicChestLayerModel;
 import artifacts.client.render.model.entity.MimicModel;
+import artifacts.common.config.ModConfig;
 import artifacts.common.entity.MimicEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
@@ -18,25 +19,52 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.fml.ModList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 public class MimicChestLayer extends LayerRenderer<MimicEntity, MimicModel> {
 
-    private final MimicChestLayerModel model;
-    public final RenderMaterial material;
+    private final MimicChestLayerModel chestModel;
+    public final RenderMaterial vanillaChestMaterial;
+    public final List<RenderMaterial> chestMaterials;
 
-
+    @SuppressWarnings("deprecation")
     public MimicChestLayer(IEntityRenderer<MimicEntity, MimicModel> entityRenderer) {
         super(entityRenderer);
 
-        model = new MimicChestLayerModel();
+        Calendar calendar = Calendar.getInstance();
+        boolean isChristmas = calendar.get(Calendar.MONTH) + 1 == 12 && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26;
+
+        chestModel = new MimicChestLayerModel();
+        chestMaterials = new ArrayList<>();
+        vanillaChestMaterial = Atlases.chooseMaterial(null, ChestType.SINGLE, isChristmas);
 
         if (ModList.get().isLoaded("lootr")) {
-            material = new RenderMaterial(AtlasTexture.LOCATION_BLOCKS, new ResourceLocation("lootr", "chest"));
+            ResourceLocation chestLocation = new ResourceLocation("lootr", "chest");
+            chestMaterials.add(new RenderMaterial(AtlasTexture.LOCATION_BLOCKS, chestLocation));
         } else {
-            Calendar calendar = Calendar.getInstance();
-            boolean isChristmas = calendar.get(Calendar.MONTH) + 1 == 12 && calendar.get(Calendar.DATE) >= 24 && calendar.get(Calendar.DATE) <= 26;
-            material = Atlases.chooseMaterial(null, ChestType.SINGLE, isChristmas);
+            if (!isChristmas && ModList.get().isLoaded("quark")) {
+                List<String> chestTypes = Arrays.asList(
+                        "oak",
+                        "spruce",
+                        "birch",
+                        "jungle",
+                        "acacia",
+                        "dark_oak",
+                        "warped",
+                        "crimson"
+                );
+
+                ResourceLocation atlas = new ResourceLocation("textures/atlas/chest.png");
+                for (String chestType : chestTypes) {
+                    ResourceLocation chestLocation = new ResourceLocation("quark", String.format("model/chest/%s/normal", chestType));
+                    chestMaterials.add(new RenderMaterial(atlas, chestLocation));
+                }
+            }
+
+            chestMaterials.add(vanillaChestMaterial);
         }
     }
 
@@ -48,13 +76,23 @@ public class MimicChestLayer extends LayerRenderer<MimicEntity, MimicModel> {
             matrixStack.mulPose(Vector3f.XP.rotationDegrees(180));
             matrixStack.translate(-0.5, -1.5, -0.5);
 
-            getParentModel().copyPropertiesTo(model);
-            model.prepareMobModel(mimic, limbSwing, limbSwingAmount, partialTicks);
-            model.setupAnim(mimic, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-            IVertexBuilder builder = material.buffer(buffer, RenderType::entityCutout);
-            model.renderToBuffer(matrixStack, builder, packedLight, LivingRenderer.getOverlayCoords(mimic, 0), 1, 1, 1, 1);
+            getParentModel().copyPropertiesTo(chestModel);
+            chestModel.prepareMobModel(mimic, limbSwing, limbSwingAmount, partialTicks);
+            chestModel.setupAnim(mimic, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+            IVertexBuilder builder = getChestMaterial(mimic).buffer(buffer, RenderType::entityCutout);
+            chestModel.renderToBuffer(matrixStack, builder, packedLight, LivingRenderer.getOverlayCoords(mimic, 0), 1, 1, 1, 1);
 
             matrixStack.popPose();
         }
+    }
+
+    private RenderMaterial getChestMaterial(MimicEntity mimic) {
+        if (!ModConfig.client.useModdedMimicTextures.get()) {
+            return vanillaChestMaterial;
+        }
+        if (chestMaterials.size() == 1) {
+            return chestMaterials.get(0);
+        }
+        return chestMaterials.get((int) (Math.abs(mimic.getUUID().getMostSignificantBits()) % chestMaterials.size()));
     }
 }
--- src/main/java/artifacts/common/config/ClientConfig.java
+++ src/main/java/artifacts/common/config/ClientConfig.java
@@ -8,22 +8,30 @@ public class ClientConfig {
     public final ForgeConfigSpec.BooleanValue modifyHurtSounds;
     public final ForgeConfigSpec.BooleanValue showFirstPersonGloves;
     public final ForgeConfigSpec.BooleanValue showTooltips;
+    public final ForgeConfigSpec.BooleanValue useModdedMimicTextures;
 
     ClientConfig(ForgeConfigSpec.Builder builder) {
         builder.push("items");
         modifyHurtSounds = builder
                 .worldRestart()
                 .comment("Whether the Kitty Slippers and Bunny Hoppers should modify the player's hurt sounds")
-                .translation(Artifacts.MODID + ".config.client.modify_hurt_sounds")
+                .translation(Artifacts.MODID + ".config.client.items.modify_hurt_sounds")
                 .define("modify_hurt_sounds", true);
         showFirstPersonGloves = builder
                 .comment("Whether models for gloves should display on the player's hand in first person")
-                .translation(Artifacts.MODID + ".config.client.show_first_person_gloves")
+                .translation(Artifacts.MODID + ".config.client.items.show_first_person_gloves")
                 .define("show_first_person_glove", true);
         showTooltips = builder
                 .comment("Whether artifacts should have a tooltip explaining their effect. These can still be found in JEI when disabled")
-                .translation(Artifacts.MODID + ".config.client.show_tooltips")
+                .translation(Artifacts.MODID + ".config.client.items.show_tooltips")
                 .define("show_tooltips", true);
+        builder.pop();
+        builder.push("Mimics");
+
+        useModdedMimicTextures = builder
+                .comment("Whether mimics should use chest textures from Lootr or Quark if these are installed")
+                .translation(Artifacts.MODID + ".config.client.mimics.use_modded_textures")
+                .define("use_modded_textures", true);
         builder.pop();
     }
 }
