516f709fdf6906d5772f878da3dc887f9e706883
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2021-09-28 22:45:07 +0200

    cleanup

--- src/main/java/artifacts/common/item/curio/belt/ObsidianSkullItem.java
+++ src/main/java/artifacts/common/item/curio/belt/ObsidianSkullItem.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvents;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import top.theillusivec4.curios.api.SlotContext;
@@ -20,20 +19,20 @@ public class ObsidianSkullItem extends CurioItem {
     }
 
     private void onLivingHurt(LivingHurtEvent event, LivingEntity wearer) {
-        if (!wearer.level.isClientSide
+        if (
+                !wearer.level.isClientSide
                 && event.getAmount() >= 1
-                && (event.getSource() == DamageSource.ON_FIRE || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.HOT_FLOOR)
-                && wearer instanceof PlayerEntity) {
+                && event.getSource().isFire()
+                && wearer instanceof PlayerEntity
+                && !((PlayerEntity) wearer).getCooldowns().isOnCooldown(this)
+        ) {
+            int cooldown = ModConfig.server.obsidianSkull.cooldown.get();
+            int fireResistanceDuration = ModConfig.server.obsidianSkull.fireResistanceDuration.get();
 
-            if (!((PlayerEntity) wearer).getCooldowns().isOnCooldown(this)) {
-                int cooldown = ModConfig.server.obsidianSkull.cooldown.get();
-                int fireResistanceDuration = ModConfig.server.obsidianSkull.fireResistanceDuration.get();
+            wearer.addEffect(new EffectInstance(Effects.FIRE_RESISTANCE, fireResistanceDuration, 0, false, true));
+            ((PlayerEntity) wearer).getCooldowns().addCooldown(this, cooldown);
 
-                wearer.addEffect(new EffectInstance(Effects.FIRE_RESISTANCE, fireResistanceDuration, 0, false, true));
-                ((PlayerEntity) wearer).getCooldowns().addCooldown(this, cooldown);
-
-                damageEquippedStacks(wearer);
-            }
+            damageEquippedStacks(wearer);
         }
     }
 
