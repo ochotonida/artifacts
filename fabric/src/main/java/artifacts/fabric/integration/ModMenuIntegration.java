4fb0c17eb51ae4a6cf60d94ae3060e1311311e37
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2023-08-04 00:04:34 +0200

    Config screen

---
+++ fabric/src/main/java/artifacts/fabric/integration/ModMenuIntegration.java
@@ -0,0 +1,14 @@
+package artifacts.fabric.integration;
+
+import artifacts.config.ModConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
