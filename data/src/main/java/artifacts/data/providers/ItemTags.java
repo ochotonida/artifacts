344ed268e44d8c9ed2522f69fb412ef0566533ec
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2023-08-15 13:11:21 +0200

    Add umbrella to origins' shields tag

--- data/src/main/java/artifacts/data/providers/ItemTags.java
+++ data/src/main/java/artifacts/data/providers/ItemTags.java
@@ -21,6 +21,7 @@ public class ItemTags extends ItemTagsProvider {
 
     public static final TagKey<Item> ARTIFACTS = TagKey.create(Registries.ITEM, Artifacts.id("artifacts"));
     public static final TagKey<Item> ORIGINS_MEAT = TagKey.create(Registries.ITEM, new ResourceLocation("origins", "meat"));
+    public static final TagKey<Item> ORIGINS_SHIELDS = TagKey.create(Registries.ITEM, new ResourceLocation("origins", "shields"));
 
     public ItemTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagsProvider.TagLookup<Block>> blockTags, @Nullable ExistingFileHelper existingFileHelper) {
         super(packOutput, lookupProvider, blockTags, Artifacts.MOD_ID, existingFileHelper);
@@ -33,9 +34,13 @@ public class ItemTags extends ItemTagsProvider {
                 .filter(item -> ForgeRegistries.ITEMS.getKey(item).getNamespace().equals(Artifacts.MOD_ID))
                 .filter(item -> item != ModItems.MIMIC_SPAWN_EGG.get()).toList().toArray(new Item[]{})
         );
+
         tag(ORIGINS_MEAT).add(
                 ModItems.EVERLASTING_BEEF.get(),
                 ModItems.ETERNAL_STEAK.get()
+        );
+        tag(ORIGINS_SHIELDS).add(
+                ModItems.UMBRELLA.get()
         );
 
         tag(net.minecraft.tags.ItemTags.PIGLIN_LOVED).add(ModItems.GOLDEN_HOOK.get());
