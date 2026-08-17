d7631f42b7a1d9448ac9bcb6ba38338fe34cf540
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2026-08-17 17:44:10 +0200

    Fix scarf of invisibility dropped from entities not respecting hideWhenInvisible config option

--- neoforge/src/main/java/artifacts/neoforge/data/EntityEquipment.java
+++ neoforge/src/main/java/artifacts/neoforge/data/EntityEquipment.java
@@ -1,9 +1,7 @@
 package artifacts.neoforge.data;
 
-import artifacts.config.value.Value;
 import artifacts.loot.ConfigValueChance;
 import artifacts.loot.IsAprilFools;
-import artifacts.registry.ModDataComponents;
 import artifacts.registry.ModItems;
 import artifacts.registry.ModLootTables;
 import com.google.common.collect.Sets;
@@ -111,11 +109,7 @@ public class EntityEquipment {
         }
         LootPool.Builder pool = LootPool.lootPool();
         for (Item item : items) {
-            if (item == ModItems.SCARF_OF_INVISIBILITY.value()) {
-                pool.add(LootTables.item(item, 1).apply(SetComponentsFunction.setComponent(ModDataComponents.HIDE_WHEN_INVISIBLE.get(), Value.of(false))));
-            } else {
-                pool.add(LootTables.item(item, 1));
-            }
+            pool.add(LootTables.item(item, 1));
         }
         addEquipment(entityType, pool);
     }
