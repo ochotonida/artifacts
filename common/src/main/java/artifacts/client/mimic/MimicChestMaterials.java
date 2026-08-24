37cd0946e821ecc20041183625004358badc760b
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2026-08-24 17:33:42 +0200

    Fix mimics rapidly switching textures

--- common/src/main/java/artifacts/client/mimic/MimicChestMaterials.java
+++ common/src/main/java/artifacts/client/mimic/MimicChestMaterials.java
@@ -74,9 +74,10 @@ public class MimicChestMaterials {
     }
 
     private Material selectRandomSprite(Material defaultSprite, List<Material> moddedSprites, MimicEntity mimic) {
-        if (mimic.getRandom().nextDouble() < 1D / (moddedSprites.size() + 1)) {
+        int spriteIndex = (int) Math.abs(mimic.getUUID().getMostSignificantBits()) % (moddedSprites.size() + 1);
+        if (spriteIndex >= moddedSprites.size()) {
             return defaultSprite;
         }
-        return moddedSprites.get((int) (Math.abs(mimic.getUUID().getMostSignificantBits()) % moddedSprites.size()));
+        return moddedSprites.get(spriteIndex);
     }
 }
